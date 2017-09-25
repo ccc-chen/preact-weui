530d27a6859303c62cdfefec72a5e66609bba21a
ch345073110 <13986142542@163.com>
2017-09-25 16:47:23 +0800

    fix init demo

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
+
+mkdir -p ./dist/style
 lessc ./src/style/index.less > ./dist/style/index.css
+
 pug -P -o ./dist/widget/form/button ./src/widget/form/button/index.pug
 pug -P -o ./dist/widget/form/list ./src/widget/form/list/index.pug
 pug -P -o ./dist ./src/index.pug
-
-mkdir -p ./dist/style
-
